1eab45eae060234a608a5584578c3847df05f4df
Joep Bernards <zkwip2@gmail.com>
2022-04-19 22:17:09 +0200

    Switched to xUnit for testing, rebuilding the test suites

--- CMDSweep/Layout/TableGrid.cs
+++ CMDSweep/Layout/TableGrid.cs
@@ -4,52 +4,59 @@ using System;
 namespace CMDSweep.Layout;
 class TableGrid
 {
-    private readonly LinearPartitioner cp;
-    private readonly LinearPartitioner rp;
-
-    public Rectangle Bounds;
+    private readonly LinearPartitioner colPart;
+    private readonly LinearPartitioner rowPart;
+    private Rectangle bounds;
 
     public TableGrid()
     {
-        cp = new();
-        rp = new();
-        Bounds = Rectangle.Zero;
+        bounds = Rectangle.Zero;
+        colPart = new();
+        rowPart = new();
     }
 
-    public int Columns => cp.Count;
-    public int Rows => rp.Count;
+    private void setBounds(Rectangle b)
+    {
+        bounds = b;
+        colPart.Range = b.HorizontalRange;
+        rowPart.Range = b.VerticalRange;
+    }
+    public Rectangle Bounds { get => bounds; set => setBounds(value); }
 
-    public void AddColumn(int conspace, int varspace, string name = "", int repeat = 1) => cp.AddPart(name, conspace, varspace, repeat);
-    public void AddRow(int conspace, int varspace, string name = "", int repeat = 1) => rp.AddPart(name, conspace, varspace, repeat);
+    public int Columns => colPart.Count;
+    public int Rows => rowPart.Count;
+
+    public void AddColumn(int conspace, int varspace, string name = "", int repeat = 1) => colPart.AddPart(name, conspace, varspace, repeat);
+    public void AddRow(int conspace, int varspace, string name = "", int repeat = 1) => rowPart.AddPart(name, conspace, varspace, repeat);
 
     public Point GetPoint(int col, int row) => new(ColStart(col), RowStart(row));
-    public Point GetPoint(string col, string row) => new(cp[col].Start, rp[row].Start);
-    public Point GetPoint(string col, int co, string row, int ro) => new(cp[col, co].Start, rp[row, ro].Start);
+    public Point GetPoint(string col, string row) => new(colPart[col].Start, rowPart[row].Start);
+    public Point GetPoint(string col, int co, string row, int ro) => new(colPart[col, co].Start, rowPart[row, ro].Start);
 
-    public Point this[int col, int row] => new(cp[col].Start, rp[row].Start);
-    public Point this[string col, int row] => new(cp[col].Start, rp[row].Start);
-    public Point this[int col, string row] => new(cp[col].Start, rp[row].Start);
-    public Point this[string col, string row] => new(cp[col].Start, rp[row].Start);
-    public Rectangle GetCell(int col, int row) => new(cp[col].Range, rp[row].Range);
+    public Point this[int col, int row] => new(colPart[col].Start, rowPart[row].Start);
+    public Point this[string col, int row] => new(colPart[col].Start, rowPart[row].Start);
+    public Point this[int col, string row] => new(colPart[col].Start, rowPart[row].Start);
+    public Point this[string col, string row] => new(colPart[col].Start, rowPart[row].Start);
+    public Rectangle GetCell(int col, int row) => new(colPart[col].Range, rowPart[row].Range);
 
-    public Rectangle Column(string name, int offset = 0) => Column(cp[name].Offset(offset));
-    public Rectangle Column(int offset) => Column(cp[offset]);
-    public Rectangle Row(string name, int offset = 0) => Row(rp[name].Offset(offset));
-    public Rectangle Row(int offset) => Row(rp[offset]);
+    public Rectangle Column(string name, int offset = 0) => Column(colPart[name].Offset(offset));
+    public Rectangle Column(int offset) => Column(colPart[offset]);
+    public Rectangle Row(string name, int offset = 0) => Row(rowPart[name].Offset(offset));
+    public Rectangle Row(int offset) => Row(rowPart[offset]);
 
-    public Rectangle ColumnSeries(string name) => new(cp.All(name), Bounds.VerticalRange);
-    public Rectangle RowSeries(string name) => new(Bounds.HorizontalRange, rp.All(name));
+    public Rectangle ColumnSeries(string name) => new(colPart.All(name), Bounds.VerticalRange);
+    public Rectangle RowSeries(string name) => new(Bounds.HorizontalRange, rowPart.All(name));
 
     public Rectangle Column(Partition col) => new(col.Range, Bounds.VerticalRange);
     public Rectangle Row(Partition row) => new(Bounds.HorizontalRange, row.Range);
 
-    public int RowStart(int row) => rp.PartStart(row);
+    public int RowStart(int row) => rowPart.PartStart(row);
 
-    public int ColStart(int col) => cp.PartStart(col);
+    public int ColStart(int col) => colPart.PartStart(col);
 
-    public int RowEnd(int row) => rp.PartEnd(row);
+    public int RowEnd(int row) => rowPart.PartEnd(row);
 
-    public int ColEnd(int col) => cp.PartEnd(col);
+    public int ColEnd(int col) => colPart.PartEnd(col);
 
     public void CenterOn(Point c) => Bounds = Bounds.CenterOn(c);
 
@@ -57,8 +64,8 @@ class TableGrid
 
     public void FitAround(int scale = 0)
     {
-        int width = cp.ConstantSum + cp.VariableSum * scale;
-        int height = rp.ConstantSum + rp.VariableSum * scale;
+        int width = colPart.ConstantSum + colPart.VariableSum * scale;
+        int height = rowPart.ConstantSum + rowPart.VariableSum * scale;
 
         Bounds = new(Bounds.Left, Bounds.Top, width, height);
     }
---
+++ CMDSweepTests/LayoutTests/TableGridTests.cs
@@ -0,0 +1,106 @@
+using Xunit;
+using CMDSweep.Layout;
+using CMDSweep.Geometry;
+using System;
+
+namespace CMDSweepTests.LayoutTests
+{
+    public class TableGridTests
+    {
+        TableGrid _sut;
+
+        public TableGridTests()
+        {
+            _sut = new TableGrid();
+        }
+
+        [Fact]
+        public void SingleCellPartitionShouldMatchTheMainBounds()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            _sut.Bounds = rect;
+            _sut.AddColumn(0, 1);
+            _sut.AddRow(0, 1);
+
+            Assert.Equal(rect, _sut.GetCell(0, 0));
+        }
+
+        [Fact]
+        public void EmptyGridShouldBeEmpty()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => {
+                _sut.GetCell(0, 0);
+            });
+
+            Assert.Equal(0, _sut.Columns);
+            Assert.Equal(0, _sut.Rows);
+            Assert.Equal(Rectangle.Zero, _sut.Bounds);
+            Assert.Equal(new Point(0, 0), _sut.GetPoint(0, 0));
+        }
+
+        [Fact]
+        public void GridWithOneRowShouldHaveOneRow()
+        {
+            _sut.AddRow(10, 0, "r1");
+
+            Assert.Equal(0, _sut.Columns);
+            Assert.Equal(1, _sut.Rows);
+            Assert.Equal(0, _sut.RowStart(0));
+            Assert.Equal(10, _sut.RowEnd(0));
+        }
+
+        [Fact]
+        public void GridWithOneColumnAndRowShouldHaveOneRow()
+        {
+            _sut.AddRow(10, 0, "r1");
+            _sut.AddColumn(15, 0, "c1");
+
+            Assert.Equal(1, _sut.Columns);
+            Assert.Equal(1, _sut.Rows);
+            Assert.Equal(0, _sut.ColStart(0));
+            Assert.Equal(15, _sut.ColEnd(0));
+
+            Assert.Equal(Point.Origin, _sut.GetPoint(0, 0));
+            Assert.Equal(new Point(15, 10), _sut.GetPoint(1, 1));
+
+            Assert.Equal(_sut.Bounds.TopLeft, _sut.GetPoint("c1", "r1"));
+            Assert.Equal(_sut.Bounds.TopLeft, _sut.GetPoint("c1", 0, "r1", 0));
+        }
+
+        [Fact]
+        public void GridWithMultipleColumns()
+        {
+            _sut.AddRow(10, 0, "r1");
+            _sut.AddColumn(15, 0, "c1"); 
+            _sut.Bounds = _sut.Bounds.Shift(10, 5);
+            _sut.AddColumn(25, 0, "c2", 4);
+
+            Assert.Equal(5, _sut.Columns);
+            Assert.Equal(1, _sut.Rows);
+
+            Assert.Equal(_sut.Bounds.TopLeft, _sut.GetPoint("c1", "r1"));
+            Assert.Equal(_sut.Bounds.TopLeft, _sut.GetPoint("c1", 0, "r1", 0));
+
+            Assert.Equal(10 + 15, _sut.GetPoint("c2", "r1").X);
+            Assert.Equal(10 + 15, _sut.GetPoint("c2", 0, "r1", 0).X);
+
+        }
+
+        [Fact]
+        public void PopulatedGridShouldNotAllowIndexingOutOfBounds()
+        {
+            Rectangle rect = new Rectangle(0, 0, 10, 10);
+            _sut.Bounds = rect;
+            _sut.AddColumn(0, 1);
+            _sut.AddRow(0, 1);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => {
+                _sut.GetCell(1, 0);
+            });
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => {
+                _sut.GetCell(0, 1);
+            });
+        }
+    }
+}
